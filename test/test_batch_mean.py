e27fa732f08d25feb974002f877039fa1e3cf94b
cgandrud <christopher.gandrud@gmail.com>
2021-08-01 16:48:33 +0200

    add actions yaml

--- test/test_batch_mean.py
+++ test/test_batch_mean.py
@@ -1,5 +1,5 @@
-import statsbatch as sb
-
+import stats_batch as sb
+import numpy as np
 
 # Test batch_mean returns the mean if prior_mean and prior_sample_size are missing
 def test_batch_mean_missing_prior_mean_prior_sample_size():
