62d3eb032b2aa374e7b21f09aa804b4fc3fe052b
cgandrud <christopher.gandrud@gmail.com>
2021-08-03 21:04:00 +0200

    ttest method added

--- test/test_batch_mean_var_t.py
+++ test/test_batch_mean_var_t.py
@@ -1,10 +1,9 @@
 from math import sqrt
 import stats_batch as sb
 import numpy as np
-import numpy.testing as npt
+from pytest import approx
 from scipy.stats import ttest_ind_from_stats
 from scipy.stats import ttest_ind
-from math import sqrt
 
 def batch_mean_var_t_test():
     n = 10_000
@@ -23,22 +22,20 @@ def batch_mean_var_t_test():
     # a -----------
     batch_2_a = a[100:n]
     a_current.update(batch_2_a)
-    npt.assert_approx_equal(a_current.mean, np.mean(a))
-    npt.assert_approx_equal(a_current.var, np.var(a), significant=4)
+    assert a_current.mean == approx(np.mean(a))
+    assert a_current.var  == approx(np.var(a), rel = 1e-3)
 
     # b -----------
     batch_2_b = b[100:n]
     b_current.update(batch_2_b)
-    npt.assert_approx_equal(b_current.mean, np.mean(b))
-    npt.assert_approx_equal(b_current.var, np.var(b), significant=4)
+    assert b_current.mean == approx(np.mean(b))
+    assert b_current.var == approx(np.var(b), rel = 1e-3)
 
     # t-test
-    batch_t = ttest_ind_from_stats(mean1=a_current.mean, mean2=b_current.mean,
-                                   std1=sqrt(a_current.var), std2=sqrt(b_current.var),
-                                   nobs1=a_current.sample_size, nobs2=b_current.sample_size)
+    batch_t = a_current.ttest_ind(b_current)
     list_t = ttest_ind(a, b)
-    assert batch_t[0] == list_t[0]
-    assert batch_t[1] == list_t[1]
+    assert batch_t[0] == approx(list_t[0])
+    assert batch_t[1] == approx(list_t[1])
 
 
  
