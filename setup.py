d5916c6e93e22e85a33fcbd630efd4dc4f7d67eb
cgandrud <christopher.gandrud@gmail.com>
2021-08-08 20:31:37 +0200

    increment to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='stats_batch',
-    version='0.0.9001',    
+    version='0.1',    
     description='Find statistics (e.g. mean and variance) using batch updating algorithms',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- stats_batch/__init__.py
+++ stats_batch/__init__.py
@@ -2,7 +2,7 @@
 Find statistics (e.g. mean and variance) using batch updating algorithms
 """
 
-__version__ = "0.0.9000"
+__version__ = "0.1"
 __author__ = 'Christopher Gandrud'
 
 from .mean_var_batch import *
